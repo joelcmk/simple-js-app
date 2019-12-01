fe6d089668051e97fe87285e0845ac0aea4dfa76
joelcmk <57118300+joelcmk@users.noreply.github.com>
2019-11-30 22:59:23 -0600

    Added variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,6 @@
 //Alert
 alert('Hello world');
 
-var myName = ('Bob');
-document.write(myName);
-
-//This is a comment
-myName = 'John Doe';
-document.write(myName);
-
-//Addition
-var simpleAddition = 7 + 11;
-document.write(simpleAddition);
-
-//Multiplication
-var size = 300;
-var doubleSize = size * 2;
-document.write(doubleSize);
-
-//Exampel 3
-var size = 100;
-var doubleSize = size * 2;
-var minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
+//Favorite food
+var favoriteFood = Tacos;
+document.write(tacos);
