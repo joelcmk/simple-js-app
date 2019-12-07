0951ac267fd2d42d6e208be04615316910fe72ee
joelcmk <57118300+joelcmk@users.noreply.github.com>
2019-12-06 19:40:55 -0600

    fixed syntac error

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ var repository = [
 ];
 for (var i = 0; i < repository.length; i++) {
     if(repository[i].height >= 1.7) {
-      document.write('<p>'+ repository[i].name + ' (height: ' +repository[i].height + ') - Wow that is big!</p>');
+      document.write('<p>'+ repository[i].name + ' (height: ' +repository[i].height + ') - Wow that\'s big!</p>');
     } else {
       document.write('<p>'+ repository[i].name + ' (height: ' +repository[i].height + ')</p>');
     }
