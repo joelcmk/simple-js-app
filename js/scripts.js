32acfb181607239072904b810ff904a760041095
joelcmk <57118300+joelcmk@users.noreply.github.com>
2019-12-07 17:53:33 -0600

    Declared function

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,15 @@ var repository = [
   {name:'Charmander', height: 1.7, type: 'pison'},
   {name:'Primeape', height: 0.5, type: 'grass'},
 ];
-for (var i = 0; i < repository.length; i++) {
-    if(repository[i].height >= 1.7) {
-      document.write('<p>'+ repository[i].name + ' (height: ' +repository[i].height + ') - Wow that\'s big!</p>');
-    } else {
-      document.write('<p>'+ repository[i].name + ' (height: ' +repository[i].height + ')</p>');
-    }
+
+function printArrayDetails(){
+  for (var i = 0; i < repository.length; i++) {
+      if(repository[i].height >= 1.7) {
+        document.write('<p>'+ repository[i].name + ' (height: ' +repository[i].height + ') - Wow that\'s big!</p>');
+      } else {
+        document.write('<p>'+ repository[i].name + ' (height: ' +repository[i].height + ')</p>');
+      }
+  }
 }
 
 
