3f4cf220a248993f5559b3b8c438444ca1849986
joelcmk <57118300+joelcmk@users.noreply.github.com>
2019-12-06 19:31:45 -0600

    Added a loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 var repository = [
-  {name:'Bulbasaur', height: 7, type: 'grass'},
-  {name:'Charmander', height: 6, type: 'pison'},
-  {name:'Primeape', height: 10, type: 'grass'},
+  {name:'Bulbasaur', height: 0.7, type: 'grass'},
+  {name:'Charmander', height: 1.7, type: 'pison'},
+  {name:'Primeape', height: 0.5, type: 'grass'},
 ];
+for (var i = 0; i < repository.length; i++) {
+    if(repository[i].height >= 1.7) {
+      document.write('<p>'+ repository[i].name + ' (height: ' +repository[i].height + ') - Wow that is big!</p>');
+    } else {
+      document.write('<p>'+ repository[i].name + ' (height: ' +repository[i].height + ')</p>');
+    }
+}
+
+
+//document.write(repository[0].name);
